6e12b8b1fb31447ab464dc79c3ce74e9ffa6b887
Josh Singh <joshsingh@Joshs-MacBook-Pro.local>
2020-12-14 11:54:59 -0500

    added text summarization

--- senta/senta.py
+++ senta/senta.py
@@ -1,13 +1,62 @@
 #!/usr/bin/env python
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.corpus import stopwords
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 import json
 from kafka import KafkaConsumer, KafkaProducer
+from string import punctuation
 from dotenv import load_dotenv
 import os
+import numpy as np
+import pandas as pd
 nltk.download('vader_lexicon')
+nltk.download('punkt')
+nltk.download('stopwords')
+
+def summarize(text):
+    #tokenize sentences
+    sentences = nltk.sent_tokenize(text)
+
+    #set stop words
+    stops = list(set(stopwords.words('english'))) + list(punctuation)
+
+    #vectorize sentences and remove stop words
+    vectorizer = TfidfVectorizer(stop_words = stops)
+
+    #transform using TFIDF vectorizer
+    trsfm=vectorizer.fit_transform(sentences)
+
+    #creat df for input article
+    text_df = pd.DataFrame(trsfm.toarray(),columns=vectorizer.get_feature_names(),index=sentences)
+
+    #declare how many sentences to use in summary
+    num_sentences = int(np.ceil(text_df.shape[0]**.5))
+
+    #find cosine similarity for all sentence pairs
+    similarities = cosine_similarity(trsfm, trsfm)
+
+    #create list to hold avg cosine similarities for each sentence
+    avgs = []
+    for i in similarities:
+        avgs.append(i.mean())
+
+    #find index values of the sentences with highest cosine similarities
+    indexes = sorted(np.argsort(avgs)[-num_sentences:])
+
+    #extract the selected sentences from the original text
+    sent_list = nltk.sent_tokenize(text)
+    sents = []
+    for i in indexes:
+        sents.append(sent_list[i].replace('\n', ''))
+
+    #join sentences together for final summary
+    summary = ' '.join(sents)
+    return summary
 
 def main():
+
     # Load .env file into environment
     load_dotenv()
     sid = SentimentIntensityAnalyzer()
@@ -52,9 +101,17 @@ def main():
         #print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
         #                                     message.offset, message.key,
         #                                     message.value))
-        polarity_scores = sid.polarity_scores(json.loads(message.value.decode('utf-8'))["value0"])
-        print(polarity_scores)
-        producer.send(producer_topic, json.dumps(polarity_scores, ensure_ascii=False).encode('utf8'))
+        
+        content_type = json.loads(message.value.decode('utf-8'))["value0"]
+        body = json.loads(message.value.decode('utf-8'))["value1"]
+        polarity_scores = sid.polarity_scores(body)
+        if content_type == "news":
+            summary = summarize(body)
+            print(summary)
+            summary_scores = sid.polarity_scores(summary)
+            producer.send(producer_topic, json.dumps({'type': content_type, 'summary': summary,'overall_sentiment': polarity_scores['compound'], 'summary_sentiment': summary_scores['compound']}, ensure_ascii=False).encode('utf8'))
+        else:
+            producer.send(producer_topic, json.dumps({'type': content_type, 'overall_sentiment': polarity_scores['compound']}, ensure_ascii=False).encode('utf8'))
 
 if __name__ == '__main__':
     main()
